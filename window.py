9c8cb75bf8b71e991e349347f7be94ea947c0f79
root <root@FIB-bCsriQNY2YA.fibia.local>
2024-08-22 21:56:55 +0200

    got a few things to look at in the window.py file..

--- window.py
+++ window.py
@@ -1,17 +1,28 @@
 from tkinter import Tk, BOTH, Canvas
 
 
-class window:
+class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.__root = Tk()
+        self.__root.geometry(f"{self.height}x{self.width}")
+        self.__root.title("Maze Solver")
+        C = Canvas(self.__root, bg="black", height=self.height, width=self.width)
+        self.running = False
 
-        root = Tk()
-        root.geometry(f"{self.height}x{self.width}")
-        root.title("Maze Solver")
+        C.pack()
+        self.__root.mainloop()
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
 
-        root.mainloop()
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
 
-
-print(window(500, 500))
+    def close(self):
+        self.running = False
