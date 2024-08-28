dab45736785ae0f2bbd9ddae555b15043e9e0ba6
root <root@FIB-bCsriQNY2YA.fibia.local>
2024-08-28 14:05:35 +0200

    gui and cell changes

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-from window import Window
+from gui import Window, Line, Point
+from cell import Cell
 
-win = Window(800,600)
-win.wait_for_close()
+
+def main():
+    win = Window(800,600)
+    
+    l = Line(Point(0,0), Point(600,600)) 
+    win.draw_line(l, "white")
+    
+    l2 = Line(Point(-50,50), Point(500,500))
+    win.draw_line(l2, "red")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    win.wait_for_close()
+
+main()
